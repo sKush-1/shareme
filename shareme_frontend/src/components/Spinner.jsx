e7aaabf1947839fa855495a8bee5a62ebb28660f
sKush-1 <kushsumit27@gmail.com>
2024-04-20 20:09:49 +0530

    Added functionality to create pin

--- shareme_frontend/src/components/Spinner.jsx
+++ shareme_frontend/src/components/Spinner.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import * as Loader from "react-loader-spinner";
+import {Circles} from "react-loader-spinner";
 
 function Spinner({ message }) {
   return (
     <div className="flex flex-col justify-center items-center w-full h-full">
-      {/* <Loader
-        type="Circles"
-        color="#00BFFF"
-        height={50}
-        width={200}
-        className="m-5"
-      /> */}
+      <Circles
+  height="80"
+  width="80"
+  color="#4fa94d"
+  ariaLabel="circles-loading"
+  wrapperStyle={{}}
+  wrapperClass=""
+  visible={true}/>
 
       <p className="text-lg text-center px-2">{message}</p>
     </div>
--- shareme_frontend/src/containers/Home.jsx
+++ shareme_frontend/src/containers/Home.jsx
@@ -8,14 +8,14 @@ import { userQuery } from '../utils/data';
 import { client } from '../client';
 import Pins from './Pins';
 import logo from '../assets/logo.png';
+import { fetchUser } from '../utils/fetchUser';
 
 const Home = () => {
   const [toggleSidebar, setToggleSidebar] = useState(false);
   const [user, setUser] = useState();
   const scrollRef = useRef(null);
-  // console.log("Home",user)
 
-  const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+  const userInfo = fetchUser();
 
   useEffect(() => {
     const query = userQuery(userInfo?.sub);
