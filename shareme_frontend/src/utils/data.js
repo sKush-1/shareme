a3df1fae175093fd741405f28fcf7c9523747868
sKush-1 <kushsumit27@gmail.com>
2024-04-22 17:48:03 +0530

    Added search bar

--- shareme_frontend/src/utils/data.js
+++ shareme_frontend/src/utils/data.js
@@ -81,6 +81,124 @@ export const userQuery = (userId) => {
     return query;
   };
 
+  export const pinDetailQuery = (pinId) => {
+    const query = `*[_type == "pin" && _id == '${pinId}']{
+      image{
+        asset->{
+          url
+        }
+      },
+      _id,
+      title, 
+      about,
+      category,
+      destination,
+      postedby->{
+        _id,
+        username,
+        image
+      },
+     save[]{
+        postedBy->{
+          _id,
+          username,
+          image
+        },
+      },
+      comments[]{
+        comment,
+        _key,
+        postedBy->{
+          _id,
+          username,
+          image
+        },
+      }
+    }`;
+    return query;
+  };
+  
+  export const pinDetailMorePinQuery = (pin) => {
+    const query = `*[_type == "pin" && category == '${pin.category}' && _id != '${pin._id}' ]{
+      image{
+        asset->{
+          url
+        }
+      },
+      _id,
+      destination,
+      postedBy->{
+        _id,
+        username,
+        image
+      },
+      save[]{
+        _key,
+        postedBy->{
+          _id,
+          username,
+          image
+        },
+      },
+    }`;
+    return query;
+  };
+
+  export const userCreatedPinsQuery = (userId) => {
+    const query = `*[ _type == 'pin' && userId == '${userId}'] | order(_createdAt desc){
+      image{
+        asset->{
+          url
+        }
+      },
+      _id,
+      destination,
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+      save[]{
+        postedBy->{
+          _id,
+          userName,
+          image
+        },
+      },
+    }`;
+    return query;
+  };
+  
+  export const userSavedPinsQuery = (userId) => {
+    const query = `*[_type == 'pin' && '${userId}' in save[].userId ] | order(_createdAt desc) {
+      image{
+        asset->{
+          url
+        }
+      },
+      _id,
+      destination,
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+      save[]{
+        postedBy->{
+          _id,
+          userName,
+          image
+        },
+      },
+    }`;
+    return query;
+  };
+
+
+
+
+
+
   export const feedQuery = `*[_type == "pin"] | order(_createdAt desc) {
     image{
       asset->{
